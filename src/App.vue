4e1eaeb8953f5193b9cab6ce586cceea90234467
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-05-09 15:54:00 -0300

    setup da calculadora com Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,78 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import Form from "./components/Form.vue";
 
-<template></template>
+const state = reactive({
+  filtro: "",
+  inputA: "",
+  inputB: "",
+  result: "Resultado",
+});
 
-<style scoped></style>
+const add = () => {
+  if (state.inputA !== "" && state.inputB !== "") {
+    return (state.result = parseInt(state.inputA) + parseInt(state.inputB));
+  } else {
+    return (state.result = "Preencher ambos os campos");
+  }
+};
+
+const subtract = () => {
+  if (state.inputA !== "" && state.inputB !== "") {
+    return (state.result = parseInt(state.inputA) - parseInt(state.inputB));
+  } else {
+    return (state.result = "Preencher ambos os campos");
+  }
+};
+
+const divide = () => {
+  if (state.inputA !== "" && state.inputB !== "") {
+    return (state.result = parseInt(state.inputA) / parseInt(state.inputB));
+  } else {
+    return (state.result = "Preencher ambos os campos");
+  }
+};
+
+const multiply = () => {
+  if (state.inputA !== "" && state.inputB !== "") {
+    return (state.result = parseInt(state.inputA) * parseInt(state.inputB));
+  } else {
+    return (state.result = "Preencher ambos os campos");
+  }
+};
+
+const filtrarOperacao = () => {
+  const { filtro } = state;
+
+  switch (filtro) {
+    case "adição":
+      return add();
+    case "subtração":
+      return subtract();
+    case "divisão":
+      return divide();
+    case "multiplicação":
+      return multiply();
+    default:
+      return state.result;
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <main class="bg-light p-3">
+      <h1 class="text-center">Calculadora</h1>
+      <Form
+        :input-a="state.inputA"
+        :input-b="state.inputB"
+        :filtro="state.filtro"
+        :result="state.result"
+        :edit-inputA="(event) => (state.inputA = event.target.value)"
+        :edit-inputB="(event) => (state.inputB = event.target.value)"
+        :filter="(event) => (state.filtro = event.target.value)"
+        :filtrar-operacao="filtrarOperacao()"
+      />
+    </main>
+  </div>
+</template>
---
+++ src/components/Form.vue
@@ -0,0 +1,65 @@
+<script setup>
+const props = defineProps([
+  "filtrarOperacao",
+  "inputA",
+  "inputB",
+  "editInputA",
+  "editInputB",
+  "filtro",
+  "filter",
+  "result",
+]);
+</script>
+
+<template>
+  <form>
+    <input
+      :value="props.inputA"
+      @change="props.editInputA"
+      type="number"
+      placeholder="Adicione um número"
+      class="me-3 form-control"
+    />
+    <span v-if="props.filtro === 'adição'" class="text-center d-block">+</span>
+    <span v-else-if="props.filtro === 'subtração'" class="text-center d-block"
+      >-</span
+    >
+    <span v-else-if="props.filtro === 'divisão'" class="text-center d-block"
+      >÷</span
+    >
+    <span
+      v-else-if="props.filtro === 'multiplicação'"
+      class="text-center d-block"
+      >x</span
+    >
+    <span v-else class="text-center d-block">*</span>
+    <input
+      :value="props.inputB"
+      @change="props.editInputB"
+      type="number"
+      placeholder="Adicione um número"
+      class="form-control"
+    />
+    <br />
+    <select class="mt-3 form-control" @change="props.filter">
+      <option value="">Escolha a operação</option>
+      <option value="adição">Adição</option>
+      <option value="subtração">Subtração</option>
+      <option value="divisão">Divisão</option>
+      <option value="multiplicação">Multiplicação</option>
+    </select>
+    <input
+      type="number"
+      disabled
+      :placeholder="props.filtrarOperacao"
+      class="mt-3 form-control"
+      :class="{ error: props.result === 'Preencher ambos os campos' }"
+    />
+  </form>
+</template>
+
+<style scoped>
+.error {
+  border: 2px solid red;
+}
+</style>
